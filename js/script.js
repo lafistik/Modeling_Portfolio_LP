6fb786a01f1e8c18fad3d5e3af68be896016df4d
lafistik <39015540+lafistik@users.noreply.github.com>
2022-12-23 21:08:47 +0900

    Add sticky nav

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
 const btns = document.querySelectorAll(".icon-mobile-nav");
 const header = document.querySelector("header");
+const heroSection = document.querySelector(".hero-section");
+const body = document.querySelector("body");
+const menuEl = document.querySelectorAll(".menu-el");
 
 btns.forEach(function (button) {
   button.addEventListener("click", function () {
     header.classList.toggle("nav-open");
   });
 });
+
+menuEl.forEach(function (el) {
+  el.addEventListener("click", function () {
+    header.classList.remove("nav-open");
+  });
+});
+
+const handler = (entries) => {
+  console.log(entries);
+
+  if (entries[0].isIntersecting) {
+    body.style.paddingTop = 0;
+    header.classList.remove("sticky");
+  } else {
+    body.style.paddingTop = header.offsetHeight + "px";
+    header.classList.add("sticky");
+  }
+};
+
+const observer = new window.IntersectionObserver(handler);
+observer.observe(heroSection);
